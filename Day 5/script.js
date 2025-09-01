5b77abca51d0b72c636ae0b52fedc56a95d5ab6c
alekh-thakur <alekhrathor8@gmail.com>
2025-09-01 16:28:12 +0530

    theme switch

---
+++ Day 5/script.js
@@ -0,0 +1,44 @@
+let themeBtn = document.querySelector("#theme-btn");
+let themeBox = document.querySelector("#theme-box");
+let knowTheme = document.querySelector("#know-theme");
+let changeImage = document.querySelector("#change-image");
+let body = document.querySelector("body");
+let isLight = true;
+
+themeBtn.addEventListener("click", () => {
+  if (isLight !== false) {
+    
+        body.style.background = "linear-gradient(135deg, #423f3fff, #263847ff)";
+    
+    
+    themeBox.style.color = "#fff";
+    themeBox.style.backgroundColor = "transparent";
+    knowTheme.style.margin = "3px";
+    knowTheme.innerHTML = "Switched to dark mode";
+    
+    changeImage.style.opacity = "0";
+    setTimeout(() => {
+        changeImage.src = "moon.png";
+        changeImage.style.opacity = "1";
+    }, 200);
+    isLight = false;
+    } 
+
+    else {
+        
+            body.style.background = "linear-gradient(135deg, #f7f6f6,#8faac2ff)";
+        
+        
+        themeBox.style.color = "#000";
+        knowTheme.innerHTML = "Switched to light mode";
+        themeBox.style.backgroundColor = "transparent";
+        
+        changeImage.style.opacity = "0";
+        setTimeout(() => {
+            changeImage.src = "sleep-mode.png";
+            changeImage.style.opacity = "1";
+        }, 200);
+        
+        isLight = true;
+    }
+});
