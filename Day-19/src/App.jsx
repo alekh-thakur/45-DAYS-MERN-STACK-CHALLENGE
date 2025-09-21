02e7a3359ac895ee38c67641a96345864180e6a6
alekh-thakur <alekhrathor8@gmail.com>
2025-09-21 17:11:00 +0530

    Day-19

---
+++ Day-19/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import Header from './header'
+
+function App() {
+  
+
+  return (
+    <>
+    <Header/>
+    </>
+  )
+}
+
+export default App
---
+++ Day-19/src/header.jsx
@@ -0,0 +1,11 @@
+function Header(){
+    return(
+        <div>
+            <h1>Alekh</h1>
+            <h2 style={{color:"#4aa5daff"}}>React developer</h2>
+            <button style={{backgroundColor:"#9fd4f3ff",color:"#ffffffff" ,border:"none"}}>Build with react</button>
+        </div>
+    )
+}
+
+export default Header;
